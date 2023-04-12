c63d45ef461de3ab1c2c5e17c90ebc5e10c71061
DMemmott <daniellememmott@hotmail.com>
2023-04-11 19:09:36 -0600

    "fixing city buttons"

--- assets/script.js
+++ assets/script.js
@@ -11,7 +11,7 @@ const history = document.querySelector("#city-list")
 searchForm.addEventListener('submit', (event) => {
     event.preventDefault();
     
-    
+    $(function displayWeather() {
     const city = searchInput.value;
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${currentAPI_KEY}`)
     .then(res => res.json())
@@ -21,7 +21,7 @@ searchForm.addEventListener('submit', (event) => {
             const element = currentData;
             const currentforecastCard = document.createElement('div');
             currentforecastCard.classList.add('currentforecast-card');
-            console.log(currentData)
+            // console.log(currentData)
 
             // Date
             let date = element.dt;
@@ -63,7 +63,7 @@ searchForm.addEventListener('submit', (event) => {
     .then(res => res.json())
     .then(data => {
         forecastContainer.innerHTML = ''
-        console.log(data.list)
+        // console.log(data.list)
         for (let index = 0; index < data.list.length; index += 8) {
             const element = data.list[index];
             const forecastCard = document.createElement('card');
@@ -107,8 +107,8 @@ searchForm.addEventListener('submit', (event) => {
             forecastCard.appendChild(descriptionEl);
 
             forecastContainer.appendChild(forecastCard)
-        }
-    })
+    }}
+    )})
     let data = document.querySelector('#search-input').value;
         const upper = data.charAt(0).toUpperCase() + data.slice(1);
         localStorage.setItem("cities", upper);
@@ -129,105 +129,7 @@ searchForm.addEventListener('submit', (event) => {
     history.addEventListener('submit2', (event) => {
         event.preventDefault();
 
-        const city = searchInput.value;
-        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${currentAPI_KEY}`)
-        .then(res => res.json())
-        .then(currentData => {
-            currentforecastContainer.innerHTML = ''
-            
-                const element = currentData;
-                const currentforecastCard = document.createElement('div');
-                currentforecastCard.classList.add('currentforecast-card');
-                console.log(currentData)
-    
-                // Date
-                let date = element.dt;
-                let formatDate = dayjs.unix(date).format('MM/DD/YYYY')
-                const dateEl = document.createElement('p');
-                dateEl.textContent = `Date: ${formatDate}`;
-                currentforecastCard.appendChild(dateEl);
-    
-                //icon
-                const iconUrl = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
-                const iconEl = document.createElement('img');
-                iconEl.src = iconUrl; 
-                currentforecastCard.appendChild(iconEl);
-                
-                // temp
-                const temp = element.main.temp;
-                const tempEl = document.createElement('p');
-                tempEl.textContent = `Temperature: ${parseInt(temp)}°F`;
-                currentforecastCard.appendChild(tempEl);
-    
-                // wind speed
-                const wind = element.wind.speed;
-                const windEl = document.createElement('p');
-                windEl.textContent = `Wind Speed: ${parseInt(wind)} MPH`;
-                currentforecastCard.appendChild(windEl);
-    
-    
-                //description
-                const description = element.weather[0].main;
-                const descriptionEl = document.createElement('p');
-                descriptionEl.textContent = `Description of weather: ${description}`;
-                currentforecastCard.appendChild(descriptionEl);
-    
-                currentforecastContainer.appendChild(currentforecastCard)
-            
-        });
-    
-        fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
-            forecastContainer.innerHTML = ''
-            console.log(data.list)
-            for (let index = 0; index < data.list.length; index += 5) {
-                const element = data.list[index];
-                const forecastCard = document.createElement('card');
-                forecastCard.classList.add('forecast-card');
-    
-                // Date
-                let date = element.dt;
-                let formatDate = dayjs.unix(date).format('MM/DD/YYYY')
-                const dateEl = document.createElement('p');
-                dateEl.textContent = `Date: ${formatDate}`;
-                forecastCard.appendChild(dateEl);
-    
-                //icon
-                const iconUrl = `https://openweathermap.org/img/w/${element.weather[0].icon}.png`;
-                const iconEl = document.createElement('img');
-                iconEl.src = iconUrl; 
-                forecastCard.appendChild(iconEl);
-    
-                // temp
-                const temp = element.main.temp;
-                const tempEl = document.createElement('p');
-                tempEl.textContent = `Temperature: ${parseInt(temp)}°F`;
-                forecastCard.appendChild(tempEl);
-    
-                // humidity         
-                const humidity = element.main.humidity;
-                const humidityEl = document.createElement('p');
-                humidityEl.textContent = `Humidity: ${humidity}%`;
-                forecastCard.appendChild(humidityEl);
-    
-                // wind speed
-                const wind = element.wind.speed;
-                const windEl = document.createElement('p');
-                windEl.textContent = `Wind Speed: ${parseInt(wind)} MPH`;
-                forecastCard.appendChild(windEl);
-    
-                //description
-                const description = element.weather[0].description;
-                const descriptionEl = document.createElement('p');
-                descriptionEl.textContent = `Description of weather: ${description}`;
-                forecastCard.appendChild(descriptionEl);
-
-                
-    
-                forecastContainer.appendChild(forecastCard)
-            }
-        })
+        displayWeather()
     })
 })
 
