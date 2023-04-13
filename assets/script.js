d46904035f698df62e5ebdf1c20eb934ac139a83
DMemmott <daniellememmott@hotmail.com>
2023-04-13 12:18:10 -0600

    "fixed layout, cities button, displayWeather"

--- assets/script.js
+++ assets/script.js
@@ -5,14 +5,13 @@ const searchForm = document.querySelector('#search-form')
 const searchInput = document.querySelector('#search-input')
 const forecastContainer = document.querySelector('#forecast-container')
 const currentforecastContainer = document.querySelector('#currentforecast-container')
+
+
 const history = document.querySelector("#city-list") 
-
-
-searchForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-    
-    $(function displayWeather() {
-    const city = searchInput.value;
+let cityHistory = []
+function displayWeather(city) {
+    // const city = searchInput.value;
+    console.log("inside displayedweather")
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${currentAPI_KEY}`)
     .then(res => res.json())
     .then(currentData => {
@@ -21,7 +20,6 @@ searchForm.addEventListener('submit', (event) => {
             const element = currentData;
             const currentforecastCard = document.createElement('div');
             currentforecastCard.classList.add('currentforecast-card');
-            // console.log(currentData)
 
             // Date
             let date = element.dt;
@@ -62,17 +60,18 @@ searchForm.addEventListener('submit', (event) => {
     fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${API_KEY}`)
     .then(res => res.json())
     .then(data => {
-        forecastContainer.innerHTML = ''
+        forecastContainer.innerHTML = '';
         // console.log(data.list)
         for (let index = 0; index < data.list.length; index += 8) {
             const element = data.list[index];
-            const forecastCard = document.createElement('card');
+            const forecastCard = document.createElement('div');
             forecastCard.classList.add('forecast-card');
 
             // Date
             let date = element.dt;
             let formatDate = dayjs.unix(date).format('MM/DD/YYYY')
             const dateEl = document.createElement('p');
+            dateEl.setAttribute("id", "date");
             dateEl.textContent = `Date: ${formatDate}`;
             forecastCard.appendChild(dateEl);
 
@@ -108,31 +107,35 @@ searchForm.addEventListener('submit', (event) => {
 
             forecastContainer.appendChild(forecastCard)
     }}
-    )})
+)}
+// event listener to bring up weather information for search input
+searchForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+      const cityName = searchInput.value;
+    displayWeather(cityName);
+// make city name upper case in local storage
     let data = document.querySelector('#search-input').value;
         const upper = data.charAt(0).toUpperCase() + data.slice(1);
-        localStorage.setItem("cities", upper);
+        cityHistory.push(upper)
+        console.log(cityHistory)
+        localStorage.setItem("cities", JSON.stringify(cityHistory));
 
-
-    let cities = localStorage.getItem("cities");
+// pull information out of local storage onto button
+    let cities = JSON.parse(localStorage.getItem("cities"));
     let cityList = document.querySelector("#city-list");
+    console.log(cities)
     
-    if (cities) {
-      let cityArray = cities.split(",");
-        cityArray.forEach(city => {
         let button = document.createElement("button");
-        button.textContent = city;
+        button.textContent = upper;
+        button.addEventListener("click", (event)=>{
+           console.log(event.target.textContent)
+        //   show weather associated with city button 
+            displayWeather(event.target.textContent)
+            
+        })
         cityList.appendChild(button);
-        
-      });
-    }
-    history.addEventListener('submit2', (event) => {
-        event.preventDefault();
-
-        displayWeather()
-    })
 })
 
-
+// show current date
 var todaysDate = dayjs();
 $('#today').text(todaysDate.format('dddd MMMM D, YYYY'));
